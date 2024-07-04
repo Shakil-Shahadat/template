644ee479e66b9992ff8f1398a626258b4af3227f
Shakil Shahadat <shakil.regi@gmail.com>
2024-07-05 05:28:00 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
-	<link rel="shortcut icon" href="src/favicon.ico">
+	<link rel="icon" href="src/favicon.ico">
 	<link rel="stylesheet" href="src/style.css">
 </head>
 <body>
